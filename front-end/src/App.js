52a7c90f68857222d2462e90b391975e5c1e42c0
Vinayak Thete <95897559+Vinayak2205@users.noreply.github.com>
2024-10-15 00:14:46 +0530

    feat: add the Skills Section to the web app.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,11 +3,13 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/Header";
+import Skills from "./components/Skills";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Skills />
     </div>
   );
 }
---
+++ front-end/src/components/Skills.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "../style/Skills.css";
+
+const Skills = () => {
+  return (
+    <section className="skills">
+      <h2>Skills</h2>
+      <ul>
+        <li>JavaScript, React.js, Java, Kotlin</li>
+        <li>Spring Boot, Micronaut, Ktor, REST APIs</li>
+        <li>PostgreSQL, Firebase, Material-UI, Bootstrap</li>
+      </ul>
+    </section>
+  );
+};
+
+export default Skills;
