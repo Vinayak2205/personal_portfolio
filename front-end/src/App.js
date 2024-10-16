1e6678540676afd4d6460815302a1c4dff965fc9
Vinayak Thete <95897559+Vinayak2205@users.noreply.github.com>
2024-10-16 22:48:54 +0530

    feat: add footer section to the portfolio

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -6,6 +6,7 @@ import Header from "./components/Header";
 import Skills from "./components/Skills";
 import Experience from "./components/Experience";
 import Projects from "./components/Projects";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Skills />
       <Experience />
       <Projects />
+      <Footer />
     </div>
   );
 }
