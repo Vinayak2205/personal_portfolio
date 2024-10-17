8900399274510a18b34a58fee5f145b7e77f6942
Vinayak Thete <95897559+Vinayak2205@users.noreply.github.com>
2024-10-17 23:47:09 +0530

    fix: removed App.css which is unused

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,7 +1,6 @@
 // src/App.js
 
 import React from "react";
-import "./App.css";
 import Header from "./components/Header";
 import Skills from "./components/Skills";
 import Experience from "./components/Experience";
