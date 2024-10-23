fa470c5ddb13a71b047d5676ece7d0d49338a8eb
Vinayak Thete <95897559+Vinayak2205@users.noreply.github.com>
2024-10-24 00:18:48 +0530

    feat: added "/about" route in the App.js file to display the about page

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Routes, Route } from "react-router-dom";
 import Homepage from "./pages/homepage";
 
 import "./app.css";
+import About from "./pages/about";
 
 const App = () => {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Homepage />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </div>
   );
