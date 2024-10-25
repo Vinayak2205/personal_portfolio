a0e04f458f122920d4521b60e809cada12ecd7de
Vinayak Thete <95897559+Vinayak2205@users.noreply.github.com>
2024-10-26 00:05:12 +0530

    feat: added a rout e "/contact" in App to display the contact component.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import "./app.css";
 import About from "./pages/about";
 import Projects from "./pages/projects";
 import Notfound from "./pages/404";
+import Contact from "./pages/contact";
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
         <Route path="/" element={<Homepage />} />
         <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
         <Route path="*" element={<Notfound />} />
       </Routes>
     </div>
